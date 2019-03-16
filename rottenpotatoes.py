085e345dc008463453ad476714a0bd73f8d6ce80
nsethupathi <nivethan.sethupathi@gmail.com>
2019-03-15 19:57:59 -0500

    testing commit/push.
    Changing welcome dialog from "Welcome to Rotten Potatoes!"
    
    to
    
    "Welcome to Rotten Potatoes! Best movie recommender on the web!"

--- rottenpotatoes.py
+++ rottenpotatoes.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Welcome to Rotten Potatoes!"
+    return "Welcome to Rotten Potatoes! Best movie recommender on the web!"
 if __name__ == "__main__":
     app.run(debug=True)
