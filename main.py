1f06d7728cee3eb937fb775ef159f86e08211996
nsethupathi <nivethan.sethupathi@gmail.com>
2019-03-16 22:46:17 -0500

    implemented request data in home.html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/", methods=['post'])
+def home_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 if __name__ == "__main__":
     app.run(debug=True)
