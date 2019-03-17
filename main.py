6dddf885fe3965b828278a0df7377ca03b5faa12
nsethupathi <nivethan.sethupathi@gmail.com>
2019-03-17 01:01:45 -0500

    re-uploading lib, templates

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ def home():
 @app.route("/", methods=['post'])
 def home_form_post():
     text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+
+    return render_template(
+        'submitted_form.html',
+        name=text, rec1 = 'Rec1', rec2 = 'Rec2', rec3 = 'Rec3', rec4 = 'Rec4', rec5 = 'Rec5')
+
 if __name__ == "__main__":
     app.run(debug=True)
